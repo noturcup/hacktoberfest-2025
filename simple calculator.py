09c18da3bf5c9724c54b8396a6457e76e4b2ef92
Nitya <114659065+noturcup@users.noreply.github.com>
2025-10-08 09:02:07 +0530

    Create simple calculator.py
    simple calculator

---
+++ simple calculator.py
@@ -0,0 +1,13 @@
+def calculator(a, b, operation):
+    if operation == 'add':
+        return a + b
+    elif operation == 'subtract':
+        return a - b
+    elif operation == 'multiply':
+        return a * b
+    elif operation == 'divide':
+        return a / b if b != 0 else "Cannot divide by zero"
+    else:
+        return "Invalid operation"
+
+print(calculator(5, 3, 'add'))  # 8
